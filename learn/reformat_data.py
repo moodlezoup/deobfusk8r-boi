bedb730b210afb42df8aa4f200516fe366d19b45
Michael Zhu <mzhu25@stanford.edu>
2017-12-09 02:34:20 -0800

    more tests and such

--- learn/reformat_data.py
+++ learn/reformat_data.py
@@ -5,7 +5,8 @@ from sklearn.feature_extraction import DictVectorizer
 db_path = 'data/processed/data.sqlite'
 sqlite_db = SqliteDict(db_path, autocommit=True)
 
-def reformat(grouping):
+
+def filteredFamilies():
     train_x = []
     train_y = []
     test_x = []
@@ -23,15 +24,45 @@ def reformat(grouping):
                 test_x.append(features)
                 test_y.append(classification)
 
+    sqlite_db['files_by_family2 train_x'] = train_x
+    sqlite_db[grouping + ' train_y'] = train_y
+    sqlite_db[grouping + ' test_x'] = test_x
+    sqlite_db[grouping + ' test_y'] = test_y
+
+
+def reformat(grouping):
+    train_x = []
+    train_y = []
+    test_x = []
+    test_y = []
+
+    table = sqlite_db['files_by_family'] if grouping == 'files_by_family2' else sqlite_db[grouping]
+    for classification, files in table.items():
+        if grouping == 'files_by_family2':
+            if len(files['train']) + len(files['test']) < 2000:
+                continue
+
+        for file_name in files['train']:
+            if file_name in sqlite_db:
+                features = sqlite_db[file_name]
+                train_x.append(features)
+                train_y.append(classification)
+        for file_name in files['test']:
+            if file_name in sqlite_db:
+                features = sqlite_db[file_name]
+                test_x.append(features)
+                test_y.append(classification)
+
     sqlite_db[grouping + ' train_x'] = train_x
     sqlite_db[grouping + ' train_y'] = train_y
     sqlite_db[grouping + ' test_x'] = test_x
     sqlite_db[grouping + ' test_y'] = test_y
 
 
-reformat('files_by_family')
-reformat('files_by_type')
-reformat('files_by_type2')
+# reformat('files_by_family')
+reformat('files_by_family2')
+# reformat('files_by_type')
+# reformat('files_by_type2')
 
 
 def vectorize(vectorizer, grouping):
@@ -39,14 +70,15 @@ def vectorize(vectorizer, grouping):
     sqlite_db[grouping + ' test_x_vectorized'] = vectorizer.transform(sqlite_db[grouping + ' test_x'])
 
 
-vectorizer = DictVectorizer(sparse=False)
-vectorizer.fit(sqlite_db['files_by_family train_x'])
-sqlite_db['vectorizer'] = vectorizer
+# vectorizer = DictVectorizer(sparse=False)
+# vectorizer.fit(sqlite_db['files_by_family train_x'])
+# sqlite_db['vectorizer'] = vectorizer
+vectorizer = sqlite_db['vectorizer']
 
-
-vectorize(vectorizer, 'files_by_family')
-vectorize(vectorizer, 'files_by_type')
-vectorize(vectorizer, 'files_by_type2')
+# vectorize(vectorizer, 'files_by_family')
+vectorize(vectorizer, 'files_by_family2')
+# vectorize(vectorizer, 'files_by_type')
+# vectorize(vectorizer, 'files_by_type2')
 
 
 sqlite_db.close()
--- learn/train_classifier.py
+++ learn/train_classifier.py
@@ -8,20 +8,48 @@ from time import time
 import logging
 from sklearn.svm import SVC
 
+
+# sci-kit learn classifiers
+from sklearn.neural_network import MLPClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, ExtraTreesClassifier, GradientBoostingClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+
+
+from xgboost import XGBClassifier
+# from sklearn.preprocessing import LabelEncoder
+
+
 db_path = 'data/processed/data.sqlite'
 
-def generateClassifier(grouping):
+def generateClassifier(grouping, clf=KNeighborsClassifier(3)):
     with SqliteDict(db_path, autocommit=True) as sqlite_db:
         X = sqlite_db[grouping + ' train_x_vectorized']
         Y = sqlite_db[grouping + ' train_y']
 
-        clf = SGDClassifier(n_jobs=-1)
-        print '=========== Train ==========='
-        predictions = model_selection.cross_val_predict(clf, X, Y, cv=5)
-        print metrics.classification_report(Y, predictions)
-        print "Accuracy: " + str(metrics.accuracy_score(Y, predictions))
-        print '============================='
-        # clf.fit(X, Y)
+        # clf = KNeighborsClassifier(n_neighbors=6, weights='distance', p=1, n_jobs=-1)
+        # clf = XGBClassifier(max_depth=4, learning_rate=0.1, n_estimators=100, objective='multi:softprob')
+        clf = ExtraTreesClassifier(criterion='entropy', max_features=0.7)
+        # clf = RandomForestClassifier()
+        # clf = DecisionTreeClassifier()
+
+        # print '=========== Train ==========='
+        # predictions = model_selection.cross_val_predict(clf, X, Y, cv=5)
+        # print metrics.classification_report(Y, predictions)
+        # print "Accuracy: " + str(metrics.accuracy_score(Y, predictions))
+        # print '============================='
+
+        clf.fit(X, Y)
+        vectorizer = sqlite_db['vectorizer']
+        feature_names = vectorizer.get_feature_names()
+        feature_importances = clf.feature_importances_
+
+        for (name, importance) in sorted(zip(feature_names, feature_importances), key = lambda x: x[1], reverse=True):
+            print name, importance
+
         #
         # print '=========== Test ============'
         # X = sqlite_db[grouping + ' test_x_vectorized']
@@ -45,11 +73,14 @@ def parameterTuning(grouping):
     #   # {'clf__C': [0.1, 1, 10, 100, 1000], 'clf__degree': [2, 3, 4], 'clf__kernel': ['poly'] }
     # ]
     pipeline = Pipeline([
-        ('clf', SGDClassifier(n_jobs=-1)),
+        ('clf', ExtraTreesClassifier(n_jobs=-1)),
     ])
-    params = [
-      {'clf__penalty': ['l1', 'l2', 'elasticnet'], 'clf__loss': ['hinge', 'log', 'modified_huber', 'squared_hinge', 'perceptron']}
-    ]
+    params = [{
+        'clf__n_estimators': [8, 10, 12],
+        'clf__criterion': ['gini', 'entropy'],
+        'clf__max_features': [0.4, 0.7, 1],
+        'clf__class_weight': [None, 'balanced'],
+    }]
 
     sqlite_db = SqliteDict(db_path, autocommit=True)
     X = sqlite_db[grouping + ' train_x_vectorized']
@@ -73,7 +104,53 @@ def parameterTuning(grouping):
     # print "Accuracy: " + str(metrics.accuracy_score(Y, predictions))
 
 
+def XGBoostClassifier(grouping):
+    pipeline = Pipeline([
+        ('clf', XGBClassifier(nthread=4)),
+    ])
+    params = [{
+        'clf__max_depth': [4],
+        'clf__learning_rate': [0.1],
+        'clf__n_estimators': [100],
+        'clf__objective': ['multi:softprob'],
+    }]
+
+    with SqliteDict(db_path, autocommit=True) as sqlite_db:
+        X = sqlite_db[grouping + ' train_x_vectorized']
+        Y = sqlite_db[grouping + ' train_y']
+
+        grid_search = model_selection.GridSearchCV(pipeline, param_grid=params, n_jobs=-1, verbose=1)
+        t0 = time()
+        grid_search.fit(X, Y)
+        print("done in %0.3fs" % (time() - t0))
+        # sqlite_db['grid_search'] = grid_search
+        # sqlite_db.close()
+
+        print("Best score: %0.3f" % grid_search.best_score_)
+        print("Best parameters set:")
+        best_parameters = grid_search.best_estimator_.get_params()
+        for param_name in params[0].keys():
+            print("\t%s: %r" % (param_name, best_parameters[param_name]))
+
+        predictions = grid_search.best_estimator_.predict(X)
+        print metrics.classification_report(Y, predictions)
+
+    # with SqliteDict(db_path, autocommit=True) as sqlite_db:
+    #     X = sqlite_db[grouping + ' train_x_vectorized']
+    #     Y = sqlite_db[grouping + ' train_y']
+    #     clf = XGBClassifier(nthread=4)
+    #     # model.fit(X, Y)
+    #
+    #     print '=========== Train ==========='
+    #     predictions = model_selection.cross_val_predict(clf, X, Y, cv=5)
+    #     print metrics.classification_report(Y, predictions)
+    #     print "Accuracy: " + str(metrics.accuracy_score(Y, predictions))
+    #     print '============================='
+
+
 if __name__ == "__main__":
     print 'learning'
-    parameterTuning('files_by_type2')
-    # generateClassifier('files_by_family')
+    # XGBoostClassifier('files_by_type2')
+    # parameterTuning('files_by_type2')
+    generateClassifier('files_by_family2')
+    # tryManyThings('files_by_family')
