e6f9307ead0247f2b894f96d16183401ba95bcca
Michael Zhu <mzhu25@stanford.edu>
2017-12-10 18:30:32 -0800

    test, some plots

---
+++ learn/test_classifier.py
@@ -0,0 +1,102 @@
+import sklearn.metrics as metrics
+from sqlitedict import SqliteDict
+from sklearn.metrics import confusion_matrix
+from sklearn.ensemble import ExtraTreesClassifier
+import itertools
+import numpy as np
+import matplotlib.pyplot as plt
+import xgboost as xgb
+
+
+db_path = 'data/processed/data.sqlite'
+
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+
+def fixFeatureName(name):
+    return name.replace('Api', '(API)').replace('api_', '(API) ')
+
+
+def testClassifier(grouping):
+        with SqliteDict(db_path, autocommit=True) as sqlite_db:
+            X = sqlite_db[grouping + ' train_x_vectorized']
+            Y = sqlite_db[grouping + ' train_y']
+
+            clf = sqlite_db[grouping + ' classifier']
+            # print '=========== Test ============'
+            # X = sqlite_db[grouping + ' test_x_vectorized']
+            # Y = sqlite_db[grouping + ' test_y']
+            # predictions = clf.predict(X)
+            # print metrics.classification_report(Y, predictions)
+            # print "Accuracy: " + str(metrics.accuracy_score(Y, predictions))
+            # print '============================='
+
+            clf.fit(X, Y)
+            vectorizer = sqlite_db['vectorizer']
+            feature_names = vectorizer.get_feature_names()
+            feature_importances = clf.feature_importances_
+
+            sorted_features = sorted(zip(feature_names, feature_importances), key=lambda x: x[1], reverse=True)[:20]
+            sorted_importances = [feature[1] for feature in sorted_features]
+            sorted_names = [fixFeatureName(feature[0]) for feature in sorted_features]
+
+            fig = plt.figure()
+            ax = fig.add_subplot(111)
+            bars = ax.bar(range(len(sorted_features)), sorted_importances, width=0.7, align="center")
+            ax.set_ylabel('Normalized importance')
+            ax.set_xticks(range(len(sorted_names)))
+            ax.set_xticklabels(sorted_names, rotation=45, ha='right')
+            plt.show()
+
+            # cnf_matrix = confusion_matrix(Y, predictions)
+            # np.set_printoptions(precision=2)
+            #
+            # class_names = ['Airpush', 'Andup', 'Boxer', 'Dowgin', 'FakeInst', 'Koler']
+            # # Plot non-normalized confusion matrix
+            # plt.figure()
+            # plot_confusion_matrix(cnf_matrix, classes=class_names, title='Confusion matrix, without normalization')
+            #
+            # # Plot normalized confusion matrix
+            # plt.figure()
+            # plot_confusion_matrix(cnf_matrix, classes=class_names, normalize=True, title='Normalized confusion matrix')
+            # plt.show()
+
+            # for (name, importance) in sorted(zip(feature_names, feature_importances), key = lambda x: x[1], reverse=True)[:10]:
+            #     print name, importance
+
+
+if __name__ == "__main__":
+    testClassifier('files_by_family2')
--- learn/train_classifier.py
+++ learn/train_classifier.py
@@ -1,23 +1,22 @@
-from sklearn.linear_model import SGDClassifier
 import sklearn.metrics as metrics
 import sklearn.model_selection as model_selection
 from sklearn.pipeline import Pipeline
 from sqlitedict import SqliteDict
+from sklearn.metrics import confusion_matrix
 from pprint import pprint
 from time import time
 import logging
-from sklearn.svm import SVC
 
 
 # sci-kit learn classifiers
 from sklearn.neural_network import MLPClassifier
+from sklearn.linear_model import SGDClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, ExtraTreesClassifier, GradientBoostingClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
-from sklearn.preprocessing import LabelEncoder
 
 
 import xgboost as xgb
@@ -25,16 +24,34 @@ import xgboost as xgb
 
 db_path = 'data/processed/data.sqlite'
 
+
 def generateClassifier(grouping, clf=ExtraTreesClassifier(criterion='entropy', max_features=0.7)):
     with SqliteDict(db_path, autocommit=True) as sqlite_db:
         X = sqlite_db[grouping + ' train_x_vectorized']
         Y = sqlite_db[grouping + ' train_y']
 
         # clf = KNeighborsClassifier(n_neighbors=6, weights='distance', p=1, n_jobs=-1)
-        # clf = xgb.XGBClassifier(max_depth=4, learning_rate=0.1, n_estimators=100, objective='multi:softprob')
-        clf = ExtraTreesClassifier(criterion='entropy', max_features=0.7)
+        # clf = xgb.XGBClassifier(max_depth=5, min_child_weight=5, learning_rate=0.1, n_estimators=70, objective='multi:softprob')
+        # clf = ExtraTreesClassifier(n_estimators=70, max_features=0.7, n_jobs=-1)
         # clf = RandomForestClassifier()
         # clf = DecisionTreeClassifier()
+
+        # clf = xgb.XGBClassifier(
+        #     learning_rate = 0.1,
+        #     n_estimators = 70,
+        #     max_depth=5,
+        #     min_child_weight=5,
+        #     gamma=0,
+        #     subsample=0.8,
+        #     colsample_bytree=0.85,
+        #     objective= 'multi:softprob',
+        #     reg_alpha=1e-5,
+        #     reg_lambda=0.1,
+        #     scale_pos_weight=1,
+        #     seed=420
+        # )
+
+        clf = sqlite_db[grouping + ' classifier']
 
         print '=========== Train ==========='
         predictions = model_selection.cross_val_predict(clf, X, Y, cv=5)
@@ -43,22 +60,6 @@ def generateClassifier(grouping, clf=ExtraTreesClassifier(criterion='entropy', m
         print '============================='
 
         # clf.fit(X, Y)
-        # vectorizer = sqlite_db['vectorizer']
-        # feature_names = vectorizer.get_feature_names()
-        # feature_importances = clf.feature_importances_
-
-        # for (name, importance) in sorted(zip(feature_names, feature_importances), key = lambda x: x[1], reverse=True):
-        #     print name, importance
-
-        #
-        # print '=========== Test ============'
-        # X = sqlite_db[grouping + ' test_x_vectorized']
-        # Y = sqlite_db[grouping + ' test_y']
-        # predictions = clf.predict(X)
-        # print metrics.classification_report(Y, predictions)
-        # print "Accuracy: " + str(metrics.accuracy_score(Y, predictions))
-        # print '============================='
-
         # print 'saving classifier'
         # sqlite_db['classifier'] = clf
 
@@ -76,80 +77,77 @@ def parameterTuning(grouping):
         ('clf', ExtraTreesClassifier(n_jobs=-1)),
     ])
     params = [{
-        'clf__n_estimators': [8, 10, 12],
-        'clf__criterion': ['gini', 'entropy'],
-        'clf__max_features': [0.4, 0.7, 1],
-        'clf__class_weight': [None, 'balanced'],
+        'clf__n_estimators': [70],
+        'clf__criterion': ['gini'],
+        'clf__max_features': [0.7],
     }]
 
     sqlite_db = SqliteDict(db_path, autocommit=True)
     X = sqlite_db[grouping + ' train_x_vectorized']
     Y = sqlite_db[grouping + ' train_y']
 
-    grid_search = model_selection.GridSearchCV(pipeline, param_grid=params, n_jobs=-1, verbose=1)
+    grid_search = model_selection.GridSearchCV(pipeline, param_grid=params, n_jobs=-1, verbose=2)
     t0 = time()
     grid_search.fit(X, Y)
     print("done in %0.3fs" % (time() - t0))
-    sqlite_db['grid_search'] = grid_search
-    sqlite_db.close()
+    # sqlite_db['grid_search'] = grid_search
+    # sqlite_db.close()
 
+    pprint(grid_search.grid_scores_)
     print("Best score: %0.3f" % grid_search.best_score_)
     print("Best parameters set:")
     best_parameters = grid_search.best_estimator_.get_params()
     for param_name in params[0].keys():
         print("\t%s: %r" % (param_name, best_parameters[param_name]))
 
-    predictions = grid_search.best_estimator_.predict(X)
-    print metrics.classification_report(Y, predictions)
+    # predictions = grid_search.best_estimator_.predict(X)
+    # print metrics.classification_report(Y, predictions)
     # print "Accuracy: " + str(metrics.accuracy_score(Y, predictions))
 
 
 def XGBoostClassifier(grouping):
     clf = xgb.XGBClassifier(
         learning_rate = 0.1,
-        n_estimators=394,
+        n_estimators = 70,
         max_depth=5,
-        min_child_weight=1,
+        min_child_weight=5,
         gamma=0,
         subsample=0.8,
-        colsample_bytree=0.8,
+        colsample_bytree=0.85,
         objective= 'multi:softprob',
+        reg_alpha=1e-5,
+        reg_lambda=0.1,
         scale_pos_weight=1,
-        seed=27
+        seed=420,
     )
-
-    # with SqliteDict(db_path, autocommit=True) as sqlite_db:
-    #     X = sqlite_db[grouping + ' train_x_vectorized']
-    #     Y = sqlite_db[grouping + ' train_y']
-    #     xgb_param = clf.get_xgb_params()
-    #     xgb_param['num_class'] = 5
-    #     xgtrain = xgb.DMatrix(X, label=LabelEncoder().fit_transform(Y))
-    #     cvresult = xgb.cv(xgb_param, xgtrain, num_boost_round=clf.get_params()['n_estimators'], nfold=5, early_stopping_rounds=50)
-    #     print cvresult.shape[0]
 
     pipeline = Pipeline([
         ('clf', clf),
     ])
     params = [{
-        'clf__max_depth': [6, 7],
-        'clf__min_child_weight': [6, 7],
-        # 'clf__learning_rate': [0.1],
-        # 'clf__n_estimators': [100],
+        # 'clf__gamma': [0.0, 0.05, 0.1]
+        # 'clf__max_depth': [4, 5, 6],
+        # 'clf__min_child_weight': [4, 5, 6],
+        'clf__learning_rate': [0.01],
+        'clf__n_estimators': [400, 1000, 2000],
         # 'clf__objective': ['multi:softprob'],
+        # 'clf__subsample': [0.75, 0.8, 0.85],
+        # 'clf__colsample_bytree': [0.75, 0.8, 0.85],
+        # 'clf__reg_lambda': [0.001, 0.01, 0.1],
     }]
 
     with SqliteDict(db_path, autocommit=True) as sqlite_db:
         X = sqlite_db[grouping + ' train_x_vectorized']
         Y = sqlite_db[grouping + ' train_y']
 
-        grid_search = model_selection.GridSearchCV(pipeline, param_grid=params, n_jobs=-1, verbose=1)
+        grid_search = model_selection.GridSearchCV(pipeline, param_grid=params, n_jobs=-1, verbose=2)
         t0 = time()
         grid_search.fit(X, Y)
         print("done in %0.3fs" % (time() - t0))
         # sqlite_db['grid_search'] = grid_search
         # sqlite_db.close()
-
-        print("Best score: %0.3f" % grid_search.best_score_)
+        pprint(grid_search.grid_scores_)
+        print("Best score: %0.5f" % grid_search.best_score_)
         print("Best parameters set:")
         best_parameters = grid_search.best_estimator_.get_params()
         for param_name in params[0].keys():
@@ -172,7 +170,8 @@ def XGBoostClassifier(grouping):
 
 
 if __name__ == "__main__":
-    print 'learning'
-    XGBoostClassifier('files_by_family2')
+    # print 'learning'
+    # XGBoostClassifier('files_by_family2')
     # parameterTuning('files_by_type2')
-    # generateClassifier('files_by_family2')
+    generateClassifier('files_by_family2')
+    # testClassifier('files_by_family2')
